fd291d0a60b163dbae9d0a9c166cbbf34c89a33e
Krishna Pilato <krishnak.pilato@gmail.com>
2024-07-19 18:50:59 +0200

    implemented typed.js

--- frontend/angular/src/app/home/home.component.ts
+++ frontend/angular/src/app/home/home.component.ts
@@ -4,17 +4,41 @@ import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIcon } from '@angular/material/icon';
+import { NgxTypedJsModule } from 'ngx-typed-js';
+import Typed from 'typed.js';
 import { environment } from '../../environment/environment';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatIcon, MatChipsModule, CommonModule, FormsModule, MatButton],
+  imports: [
+    MatIcon,
+    MatChipsModule,
+    CommonModule,
+    FormsModule,
+    MatButton,
+    NgxTypedJsModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  languages: string[] = ['Java', "Python", "JavaScript", "TypeScript"];
+  private languages: string[] = ['Java', 'Python', 'JavaScript', 'TypeScript'];
+
+  ngOnInit() {
+    const typed = new Typed('.typed-element', {
+      strings: this.languages,
+      typeSpeed: 100,
+      backSpeed: 20,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true,
+      fadeOut: false,
+      fadeOutDelay: 1500,
+      backDelay: 1500,
+      smartBackspace: true
+    }).start();
+  }
 
   public viewDevTools(): void {
     console.log('Opening developer tools...');
