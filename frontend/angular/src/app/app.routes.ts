a201ac329cdcd1d83df10fee54b75be5226246e1
Krishna Pilato <krishnak.pilato@gmail.com>
2024-07-20 08:36:40 +0200

    removed contact component

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent, canActivate: [AuthGuard] }, 
   { path: 'auth/signup', component: RegistrationComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'contact', component: ContactComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
