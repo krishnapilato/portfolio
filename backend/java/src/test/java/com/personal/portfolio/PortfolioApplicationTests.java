a544364bd376c7b562db028c33cb28e22c0c4502
Khova Krishna Pilato <krishnak.pilato@gmail.com>
2024-10-28 13:44:20 +0100

    trying new solutions

--- backend/java/src/test/java/com/personal/portfolio/PortfolioApplicationTests.java
+++ backend/java/src/test/java/com/personal/portfolio/PortfolioApplicationTests.java
@@ -3,7 +3,7 @@ package com.personal.portfolio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PortfolioApplication.class)
 class PortfolioApplicationTests {
 
 	@Test
