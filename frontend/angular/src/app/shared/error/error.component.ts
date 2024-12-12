a9603dac4c727113995329848246f0c75fe288aa
Khova Krishna Pilato <krishnak.pilato@gmail.com>
2024-12-12 18:17:19 +0100

    bug fixing

--- frontend/angular/src/app/shared/error/error.component.ts
+++ frontend/angular/src/app/shared/error/error.component.ts
@@ -4,24 +4,33 @@ import { Component } from '@angular/core';
   selector: 'app-error',
   template: `
     <div
-      class="container my-5 d-flex align-items-center justify-content-center"
-      style="min-height: 70vh;"
+      class="flex items-center justify-center min-h-screen bg-gradient-to-r from-red-500 via-red-600 to-red-700 bg-cover bg-center"
     >
-      <div class="text-center">
+      <div class="text-center text-white">
         <img
           src="page_not_found.svg"
           alt="Page Not Found"
-          class="img-fluid mb-4"
+          class="mx-auto mb-4"
           width="200"
           height="200"
         />
-        <h1 class="display-4 text-danger mb-3">404 - Not Found</h1>
-        <p class="lead text-secondary mb-4">
-          The page you're looking for doesn't exist or an error has occurred.
-        </p>
-        <a routerLink="/" class="btn btn-primary">
-          <i class="fa fa-home me-2"></i> Go Back Home
+        <h1 class="text-4xl font-bold mb-3">404 - Not Found</h1>
+        <p class="text-lg mb-4">The page you're looking for doesn't exist.</p>
+        <a
+          routerLink="/"
+          class="inline-flex items-center px-6 py-3 bg-blue-800 text-white font-semibold rounded-md shadow-lg hover:bg-blue-700"
+        >
+          <i class="fa fa-home mr-2"></i> Go Back Home
         </a>
+      </div>
+      <!-- Copyright Notice -->
+      <div class="absolute bottom-2 right-4 text-sm text-xs text-gray-100">
+        <a
+          href="https://unsplash.com/@unsplash"
+          class="underline"
+          target="_blank"
+          >Photos by Unsplash</a
+        >
       </div>
     </div>
   `,
