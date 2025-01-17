d757b0f8144713af3084c3f92179b6a76316c658
Krishna Pilato <krishnak.pilato@gmail.com>
2025-01-17 09:21:57 +0100

    Add SkillsGalaxy section with scroll animations; update styles and hide scrollbar for carousel

--- frontend/angular/src/app/skills-galaxy/skills-galaxy.component.ts
+++ frontend/angular/src/app/skills-galaxy/skills-galaxy.component.ts
@@ -1,8 +1,45 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-skills-galaxy',
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './skills-galaxy.component.html',
   styleUrls: ['./skills-galaxy.component.css'],
 })
-export class SkillsGalaxyComponent {}
+export class SkillsGalaxyComponent {
+  ngOnInit(): void {
+    this.setupScrollAnimation();
+  }
+
+  private setupScrollAnimation(): void {
+    const section = document.getElementById('skills-intro');
+
+    gsap.fromTo(
+      section,
+      {
+        scale: 0.5,
+        opacity: 0,
+        transformOrigin: 'center center',
+      },
+      {
+        scale: 1,
+        opacity: 1,
+        transformOrigin: 'center center',
+        scrollTrigger: {
+          trigger: section,
+          start: 'top bottom',
+          end: 'top center',
+          scrub: true,
+        },
+        ease: 'power2.inOut',
+      }
+    );
+  }
+}
