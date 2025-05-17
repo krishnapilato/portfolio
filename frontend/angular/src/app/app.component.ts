948068a580a02cf50d6bbb5a94f14d6da674a06f
Khova Krishna Pilato <krishnak.pilato@gmail.com>
2025-05-17 21:06:30 +0200

    bug fixing

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavigationComponent } from './navigation/navigation.component';
-import { HeroComponent } from './hero/hero.component';
-import { ContactComponent } from './contact/contact.component';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeroComponent } from './hero/hero.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavigationComponent, HeroComponent, AboutMeComponent, ProjectsComponent, ContactComponent],
+  imports: [HeroComponent, AboutMeComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements AfterViewInit {
+  title = 'portfolio';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const cursor = document.getElementById('customCursor');
+      if (cursor) {
+        document.addEventListener('mousemove', e => {
+          cursor.style.top = `${e.clientY}px`;
+          cursor.style.left = `${e.clientX}px`;
+        });
+      }
+    }
+  }
 }
