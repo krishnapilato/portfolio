7ce9cb8b5c9ca3d759836a757292db2ed8499d01
Khova Krishna Pilato <krishnak.pilato@gmail.com>
2025-05-20 15:30:43 +0200

    bug fixing

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component } from '@angular/core';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeroComponent } from './hero/hero.component';
@@ -11,20 +10,6 @@ import { ProjectsComponent } from './projects/projects.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'portfolio';
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      const cursor = document.getElementById('customCursor');
-      if (cursor) {
-        document.addEventListener('mousemove', e => {
-          cursor.style.top = `${e.clientY}px`;
-          cursor.style.left = `${e.clientX}px`;
-        });
-      }
-    }
-  }
 }
--- frontend/angular/src/main.ts
+++ frontend/angular/src/main.ts
@@ -1,15 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-
-AOS.init({
-  duration: 1000,
-  easing: 'ease-in-out',
-  once: true,
-  delay: 100,
-});
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
